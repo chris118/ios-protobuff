209bf94c135a82d670de25e17bf1605068467df5
wangxiaopeng <wangxiaopeng@putao.com>
2017-06-05 12:56:44 +0800

    Initial Commit

---
+++ ProtocolBufferDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProtocolBufferDemo
+//
+//  Created by xiaopeng on 2017/6/5.
+//  Copyright © 2017年 putao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProtocolBufferDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProtocolBufferDemo
+//
+//  Created by xiaopeng on 2017/6/5.
+//  Copyright © 2017年 putao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
